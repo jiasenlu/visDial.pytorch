3b6cc68a2c8ce572ce12046add7147324f1e7b12
Jiasen Lu <jlu347@jarvis.cc.gatech.edu>
2017-10-05 19:01:32 -0400

    fix typo

--- script/donwload.py
+++ script/donwload.py
@@ -5,7 +5,7 @@ import json
 def download_model(path):
     os.system('wget https://filebox.ece.vt.edu/~jiasenlu/codeRelease/visDial.pytorch/model/HCIAE-D-MLE.pth -P %s' %(path))
     os.system('wget https://filebox.ece.vt.edu/~jiasenlu/codeRelease/visDial.pytorch/model/HCIAE-G-MLE.pth -P %s' %(path))
-    os.system('wget https://filebox.ece.vt.edu/~jiasenlu/codeRelease/visDial.pytorch/model/HCIAE-G-DIS -P %s' %(path))
+    os.system('wget https://filebox.ece.vt.edu/~jiasenlu/codeRelease/visDial.pytorch/model/HCIAE-G-DIS.pth -P %s' %(path))
 
 def download_feat(path):
     os.system('wget https://filebox.ece.vt.edu/~jiasenlu/codeRelease/visDial.pytorch/data/vdl_img_vgg.h5 -P %s' %(path))
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     model_path = os.path.join(params['path'], 'save')
     download_model(model_path)
 
-    data_path = os.path.join(params['path'], 'data')
-    download_feat(data_path)
+    # data_path = os.path.join(params['path'], 'data')
+    # download_feat(data_path)
 
     print('Download finished ...')
